dff318aeef9062e383b4bf638154c6f3de43494e
bhendi-boi <bhendiisgreen@gmail.com>
2022-11-19 12:01:02 +0530

    title update

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ const Title = ({ name, htmlID }) => {
   return (
     <h2
       id={htmlID}
-      className="capitalize text-center text-5xl font-medium text-white my-2"
+      className="capitalize mx-4 my-4 text-5xl font-medium text-blue-500"
     >
       {name}
     </h2>
