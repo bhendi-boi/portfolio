d5e310f74d5e62e4dc00df406258c2f1c534f345
bhendi-boi <bhendiisgreen@gmail.com>
2022-11-19 11:12:38 +0530

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,7 @@ import ScrollButton from "./ScrollButton";
 const Footer = ({ handleScrollToTop }) => {
   return (
     <footer className="bg-background py-4 flex justify-center gap-1 text-gray-300 opacity-60 items-center">
-      <p>&copy;{new Date().getFullYear()} Jyothikrishna</p>
-      <p> All rights reserved.</p>
+      <p>Copyright &copy; {new Date().getFullYear()} Jyothikrishna</p>
       <ScrollButton handleScrollToTop={handleScrollToTop} />
     </footer>
   );
