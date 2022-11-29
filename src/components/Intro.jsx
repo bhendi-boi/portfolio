3d2bff716c9a8274a8217c2ade3a48f2ada1f784
bhendi-boi <bhendiisgreen@gmail.com>
2022-11-29 21:37:11 +0530

    picture intro section update without src

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,12 +7,12 @@ const Intro = ({ scrollRef }) => {
   return (
     <section className="h-[calc(100vh-4.5rem)] flex flex-col justify-evenly items-center border-b-2 border-slate-50 border-opacity-20">
       <div className="flex flex-col items-center gap-4">
-        <picture className="bg-yellow-300 rounded-full h-52 w-52">
-          {/* <img
+        <picture className="overflow-hidden bg-yellow-300 rounded-full h-52 w-52">
+          <img
             src={imageSrc}
             alt="Jyothikrishna image"
-            className="object-contain w-full"
-          /> */}
+            className="object-cover aspect-square"
+          />
         </picture>
         <div>
           <p className="pb-2 text-3xl font-medium text-center text-white">
