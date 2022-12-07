de4da9f3b47ee51968798bea637fb75ea629206c
bhendi-boi <bhendiisgreen@gmail.com>
2022-12-07 11:55:27 +0530

    added font to name in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,8 @@ const Footer = ({ handleScrollToTop }) => {
   return (
     <footer className="flex items-center justify-center gap-1 py-4 text-gray-300 bg-background opacity-60">
       <p>
-        Copyright &copy; {new Date().getFullYear()} Jyothikrishna{" "}
+        Copyright &copy; {new Date().getFullYear()}{" "}
+        <span className="font-medium">Jyothikrishna</span>{" "}
         <span className="hidden sm:inline">All rights reserved.</span>{" "}
       </p>
       <ScrollButton handleScrollToTop={handleScrollToTop} />
