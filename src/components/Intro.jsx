c1f6b481bc8e1757e65c89578335b0d5fc6f3694
bhendi-boi <bhendiisgreen@gmail.com>
2022-12-12 09:26:28 +0530

    added download cv functionality

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,3 +1,5 @@
+import { BiLinkExternal } from "react-icons/bi";
+
 const imageSrc =
   "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
 
@@ -21,6 +23,13 @@ const Intro = () => {
           </p>
         </div>
       </div>
+      <a
+        className="inline-flex items-center gap-2 px-4 py-2 font-bold uppercase border-2 rounded-md outline-none cursor-pointer text-nav-background border-neutral-50 bg-neutral-50 hover:bg"
+        href="../assets/cv.pdf"
+        download
+      >
+        full cv as pdf <BiLinkExternal className="text-lg font-medium" />
+      </a>
     </section>
   );
 };
