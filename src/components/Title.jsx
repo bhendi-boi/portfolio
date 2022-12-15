f9aa960daa85914b72ac6d1e555bfbb76c326538
bhendi-boi <bhendiisgreen@gmail.com>
2022-12-15 22:25:14 +0530

    changed h3 to h2 in title for better seo

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,8 @@
 const Title = ({ name }) => {
   return (
-    <h3 className="mx-4 my-4 text-5xl font-semibold text-blue-500 capitalize">
+    <h2 className="mx-4 my-4 text-5xl font-semibold text-blue-500 capitalize">
       {name}
-    </h3>
+    </h2>
   );
 };
 
