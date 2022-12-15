251e069104a24a882ede2a56a936838d0fc6c7a7
bhendi-boi <bhendiisgreen@gmail.com>
2022-12-15 22:53:29 +0530

    added margin to project cards added padding at the end of projects section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,14 @@
 const ProjectCard = ({ name, stack, details, url, imageURL }) => {
+  // TODO!
+  // making the images fit in the container
   return (
-    <article className="relative h-full overflow-hidden rounded-md shadow-2xl sm:w-96 md:w-full bg-slate-800 text-slate-200 border-slate-300">
+    <article className="mx-2 text-gray-200 rounded-md shadow-2xl sm:w-96 md:w-full bg-slate-800">
       <picture className="flex flex-col gap-2 overflow-hidden bg-yellow-300 h-60">
         <a target="_blank" href={url} className="border-none">
           <img src={imageURL} alt={`${name} photo`} className="object-cover" />
         </a>
       </picture>
-      <h3 className="my-3 text-3xl font-medium text-center text-slate-50">
+      <h3 className="my-3 text-2xl font-medium text-center text-neutral-50">
         {name}
       </h3>
       <p className="mx-6 min-h-[3rem] text-base">{details}</p>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = ({ scrollRef }) => {
       className="px-2 border-b-2 border-slate-50 border-opacity-20"
     >
       <Title name="projects" />
-      <section className="grid gap-6 my-4 md:gap-4 md:mx-0 sm:place-items-center md:grid-cols-2 ">
+      <section className="grid gap-6 my-4 md:gap-4 md:mx-0 sm:place-items-center md:grid-cols-2 last:pb-4">
         {data.map((project) => {
           return (
             <ProjectCard
