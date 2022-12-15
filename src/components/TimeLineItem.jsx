274cb1182ffc4d17580e4515a33609d2373d7f90
bhendi-boi <bhendiisgreen@gmail.com>
2022-12-15 22:49:07 +0530

    changed font-size of titleprop in timeline section

--- src/components/TimeLineItem.jsx
+++ src/components/TimeLineItem.jsx
@@ -4,7 +4,7 @@ const TimeLineItem = ({ title, start, end, details, links, category }) => {
   return (
     <article className="py-2 pl-4 border-l-2 border-gray-300">
       <div className="flex flex-col relative after:content-[''] after:w-5 after:h-5 after:rounded-full after:bg-gray-200 after:absolute after:-left-[26px] after:top-1">
-        <h3 className="text-xl font-medium text-neutral-50">{title}</h3>
+        <h3 className="text-2xl font-medium text-neutral-50">{title}</h3>
         <p className="inline-flex gap-4 text-sm text-gray-400 capitalize">
           {category}
           <span>
