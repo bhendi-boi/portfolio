b02a09b20d0a223791f1e735d6b3d5a655c91183
bhendi-boi <bhendiisgreen@gmail.com>
2022-12-23 17:07:34 +0530

    data files are moved to assets

---
+++ src/assets/Projects.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: "Expense Tracker",
+    stack: ["react", "tailwind", "firebase"],
+    details:
+      "It's a work in progress. This project serves as a tool to keep track of your expenditure.",
+    url: "https://expense-tracker-react-5eaa6.web.app/",
+    imageURL: "https://placeimg.com/400/400",
+  },
+  {
+    name: "Alumni Portal",
+    stack: ["react", "tailwind", "motion"],
+    details:
+      "It's a website I built for alumni affairs IIITDM Kancheepuram. Also I got to manage a team of 3 members during the project. I worked mostly on the front end part of the website. This is the first real life project I worked on. It is mostly a static website but it also has a section where logged in users can ask and answer questions (similar to stackoverflow).",
+    url: "#",
+    imageURL: "https://placeimg.com/400/400",
+  },
+  {
+    name: "My Portfolio",
+    stack: ["react", "tailwind", "motion"],
+    details:
+      "The website which you are currently on is the outcome of this project.",
+    url: "https://bhendi-boi.github.io/portfolio/",
+    imageURL: "https://placeimg.com/400/400",
+  },
+];
---
+++ src/assets/TimeLine.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    title: "Bachelors in Electronics and Communication",
+    start: 2021,
+    end: 2025,
+    category: "education",
+    details:
+      "I am my pursuing my Bachelors from IIITDM, Kancheepuram. I am currently in my sophomore year in college.",
+  },
+  {
+    title: "React Developer",
+    start: 2022,
+    end: null,
+    category: "skill",
+    details:
+      "I am a React developer since 2022. You can check my work by visiting Projects section or my Github profile",
+    links: ["https://www.github.com/bhendi-boi"],
+  },
+  {
+    title: "Joint Core - PRIT",
+    start: 2022,
+    end: null,
+    category: "voluntering",
+    details:
+      "I voluntereed as a Joint Core for PRIT division (Projects and Industrial Talks) of CS Club IIITDM, Kancheepuram. ",
+    links: ["https://www.github.com/bhendi-boi"],
+  },
+];
