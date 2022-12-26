d822556619a7c0b36b90368cfc53f543793693d2
bhendi-boi <bhendiisgreen@gmail.com>
2022-12-26 15:22:27 +0530

    titles are changed according to refactoring ui

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ const Title = ({ name }) => {
   return (
     <h2
       id={name}
-      className="my-4 text-5xl font-title font-semibold text-blue-700 dark:text-blue-500 "
+      className="my-4 text-4xl font-title font-semibold uppercase text-gray-700 dark:text-neutral-200"
     >
       {name}
     </h2>
